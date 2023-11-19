00966e6c50fd59b68990cd50f7bc02078997601f
Vengral <105657786+Vengral@users.noreply.github.com>
2023-11-19 21:33:41 +0100

    Create  Are they the "same"?.py

---
+++ Are they the "same"?.py
@@ -0,0 +1,34 @@
+Given two arrays a and b write a function comp(a, b) (orcompSame(a, b)) that checks whether the two arrays have the "same" elements, with the same multiplicities (the multiplicity of a member is the number of times it appears). "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
+
+Examples
+Valid arrays
+a = [121, 144, 19, 161, 19, 144, 19, 11]  
+b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
+comp(a, b) returns true because in b 121 is the square of 11, 14641 is the square of 121, 20736 the square of 144, 361 the square of 19, 25921 the square of 161, and so on. It gets obvious if we write b's elements in terms of squares:
+
+a = [121, 144, 19, 161, 19, 144, 19, 11] 
+b = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]
+Invalid arrays
+If, for example, we change the first number to something else, comp is not returning true anymore:
+
+a = [121, 144, 19, 161, 19, 144, 19, 11]  
+b = [132, 14641, 20736, 361, 25921, 361, 20736, 361]
+
+
+def comp(array1, array2):
+
+    if array1 and array2:
+        array3 = [i**2 for i in array1]
+    elif array1==array2:
+        return True
+    else:
+        return False
+    
+    array2.sort()
+    array3.sort()
+    
+    if array2 == array3:
+        return True
+    else:
+        return False
+    # your code
