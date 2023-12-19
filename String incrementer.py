2eafec0857cce5f3499051e134603fe08c57630d
Vengral <105657786+Vengral@users.noreply.github.com>
2023-12-19 17:51:32 +0100

    Create String incrementer.py

---
+++ String incrementer.py
@@ -0,0 +1,29 @@
+Your job is to write a function which increments a string, to create a new string.
+
+If the string already ends with a number, the number should be incremented by 1.
+If the string does not end with a number. the number 1 should be appended to the new string.
+Examples:
+
+foo -> foo1
+
+foobar23 -> foobar24
+
+foo0042 -> foo0043
+
+foo9 -> foo10
+
+foo099 -> foo100
+
+Attention: If the number has leading zeros the amount of digits should be considered.
+
+
+  def increment_string(strng):
+    
+    head = strng.rstrip("0123456789")
+    tail = strng[len(head):]
+    
+    if len(tail) ==0:
+        return head+"1"
+    else:
+        number = str(int(tail) + 1).zfill(len(tail))
+        return head+str(number)
